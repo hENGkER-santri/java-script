44f721b4c5823c4c541e1240ab74a0f51d14bef7
hENGkER-santri <ivan.raditya@student.alhazen.academy>
2024-10-18 13:58:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var nama = "Ivan";
+console.log(nama);
+
+
+let umur = 13;
+console.log(umur);
+
+const alamat = Depok;
+console.log(alamat);
+
+angka=4;
+angka2=8;
+console.log(angka*angka2);
+
+//membuat kalkulator sederhana//
+let number1 = prompt("Masukkan Angka Pertama:");
+let number2 = prompt("Masukkan Angka Ke dua:");
+
+let result = parseInt(number1) + (number2);
+alert(result);
+
+//array
+fruit=["banana","apple","orange","durian"];
+
+alert(fruit(2));
